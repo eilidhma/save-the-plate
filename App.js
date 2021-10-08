1556d1fce6af30297393762ccf8d2c9dbafb1fdc
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2021-10-08 11:19:01 -0700

    help

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>testing123AHHHH</Text>
+      <Text>help</Text>
       <StatusBar style="auto" />
     </View>
   );
