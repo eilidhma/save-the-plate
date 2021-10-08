85c7485d96d572e56f56ce5224b506f12c702371
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2021-10-08 11:16:45 -0700

    test2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>testing 123</Text>
+      <Text>testing123AHHHH</Text>
       <StatusBar style="auto" />
     </View>
   );
