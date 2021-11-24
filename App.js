3e551840382a23d26e957cbcec9980068a360af1
Alex Martin <72417540+alexm1612@users.noreply.github.com>
2021-11-24 10:41:15 -0800

    added animation at the beginning

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
 import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import styled from 'styled-components';
+import LottieView from 'lottie-react-native';
 
 // customer pages
 import Home from './pages/customer/home';
@@ -41,7 +42,7 @@ function Landing({ navigation }) {
 
   setTimeout(() => { 
     navigation.navigate('Login')
-  }, 4000)
+  }, 3010)
 
   if (!fontsLoaded) {
     return <LinearGradient colors={['#F3AE81', '#E94168']} style={styles.container}>
@@ -51,7 +52,18 @@ function Landing({ navigation }) {
 
     <LinearGradient colors={['#F3AE81', '#E94168']} style={styles.container}>
       <Text style={{color:'white', fontFamily:'Raleway_700Bold', fontSize:32}}>SaveThePlate</Text>
-      <Image style={{width:100, height:100, marginTop:20}} source={logo}/>
+      <LottieView
+          ref={(ref) => {
+            anim = ref;
+          }}
+          style={{
+            width:'100%',
+            alignItems:'center'
+          }}
+          source={require('./assets/logo.json')}
+          autoPlay={true}
+          loop={false}
+        />
     </LinearGradient>
   );
   }
