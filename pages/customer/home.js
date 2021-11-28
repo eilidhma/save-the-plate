07721b5b828f6582d1ccc1915e7a0186e3dc3395
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2021-11-28 01:26:17 -0800

    UI tweaks and more mapping :')

--- pages/customer/home.js
+++ pages/customer/home.js
@@ -203,7 +203,6 @@ export default function Home({
       const result = await axios.get('/listed.php');
       if(!isUnmount){
         setListedData(result.data);
-        console.log(result.data)
       }
     
     })();
@@ -258,7 +257,294 @@ export default function Home({
   const [rest, setRestaurant] = useState()
   const [oldPrice, setOldPrice] = useState()
   const [newPrice, setNewPrice] = useState()
+  const [name, setName] = useState()
   const [cartItems, setCartItems] = useState([])
+
+
+  // ----- FILTERS START ------
+
+
+    // !--------------------- CUISINE --------------------!
+    // ITALIAN 
+    const [c1Color, setC1Color] = useState("white")
+    const [c1TextColor, setC1TextColor] = useState("#ff1a44")
+    const [c1Selected, setC1Selected] = useState(false)
+    const SelectCuisine1 = () => {
+      setC1Selected(!c1Selected)
+      
+      if(c1Selected === false){
+        setC1Color("#ff1a44")
+        setC1TextColor("white")
+      } else {
+        setC1Color("white")
+        setC1TextColor("#ff1a44")
+      }
+    }
+    // MEXICAN
+    const [c2Color, setC2Color] = useState("white")
+    const [c2TextColor, setC2TextColor] = useState("#ff1a44")
+    const [c2Selected, setC2Selected] = useState(false)
+    const SelectCuisine2 = () => {
+      setC2Selected(!c2Selected)
+      
+      if(c2Selected === false){
+        setC2Color("#ff1a44")
+        setC2TextColor("white")
+      } else {
+        setC2Color("white")
+        setC2TextColor("#ff1a44")
+      }
+    }
+    // VIETNAMESE
+    const [c3Color, setC3Color] = useState("white")
+    const [c3TextColor, setC3TextColor] = useState("#ff1a44")
+    const [c3Selected, setC3Selected] = useState(false)
+    const SelectCuisine3 = () => {
+      setC3Selected(!c3Selected)
+      
+      if(c3Selected === false){
+        setC3Color("#ff1a44")
+        setC3TextColor("white")
+      } else {
+        setC3Color("white")
+        setC3TextColor("#ff1a44")
+      }
+    }
+    // AMERICAN
+    const [c4Color, setC4Color] = useState("white")
+    const [c4TextColor, setC4TextColor] = useState("#ff1a44")
+    const [c4Selected, setC4Selected] = useState(false)
+    const SelectCuisine4 = () => {
+      setC4Selected(!c4Selected)
+      
+      if(c4Selected === false){
+        setC4Color("#ff1a44")
+        setC4TextColor("white")
+      } else {
+        setC4Color("white")
+        setC4TextColor("#ff1a44")
+      }
+    }
+
+    // !--------------------- DISTANCE --------------------!
+    // 200m 
+    const [dist1Color, setDist1Color] = useState("white")
+    const [dist1TextColor, setDist1TextColor] = useState("#ff1a44")
+    const [dist1Selected, setDist1Selected] = useState(false)
+    const SelectDistance1 = () => {
+      setDist1Selected(!dist1Selected)
+      
+      if(dist1Selected === false){
+        setDist1Color("#ff1a44")
+        setDist1TextColor("white")
+      } else {
+        setDist1Color("white")
+        setDist1TextColor("#ff1a44")
+      }
+    }
+    // 500m
+    const [dist2Color, setDist2Color] = useState("white")
+    const [dist2TextColor, setDist2TextColor] = useState("#ff1a44")
+    const [dist2Selected, setDist2Selected] = useState(false)
+    const SelectDistance2 = () => {
+      setDist2Selected(!dist2Selected)
+      
+      if(dist2Selected === false){
+        setDist2Color("#ff1a44")
+        setDist2TextColor("white")
+      } else {
+        setDist2Color("white")
+        setDist2TextColor("#ff1a44")
+      }
+    }
+    // 1km
+    const [dist3Color, setDist3Color] = useState("white")
+    const [dist3TextColor, setDist3TextColor] = useState("#ff1a44")
+    const [dist3Selected, setDist3Selected] = useState(false)
+    const SelectDistance3 = () => {
+      setDist3Selected(!dist3Selected)
+      
+      if(dist3Selected === false){
+        setDist3Color("#ff1a44")
+        setDist3TextColor("white")
+      } else {
+        setDist3Color("white")
+        setDist3TextColor("#ff1a44")
+      }
+    }
+    // 5km
+    const [dist4Color, setDist4Color] = useState("white")
+    const [dist4TextColor, setDist4TextColor] = useState("#ff1a44")
+    const [dist4Selected, setDist4Selected] = useState(false)
+    const SelectDistance4 = () => {
+      setDist4Selected(!dist4Selected)
+      
+      if(dist4Selected === false){
+        setDist4Color("#ff1a44")
+        setDist4TextColor("white")
+      } else {
+        setDist4Color("white")
+        setDist4TextColor("#ff1a44")
+      }
+    }
+    // 10km
+    const [dist5Color, setDist5Color] = useState("white")
+    const [dist5TextColor, setDist5TextColor] = useState("#ff1a44")
+    const [dist5Selected, setDist5Selected] = useState(false)
+    const SelectDistance5 = () => {
+      setDist5Selected(!dist5Selected)
+      
+      if(dist5Selected === false){
+        setDist5Color("#ff1a44")
+        setDist5TextColor("white")
+      } else {
+        setDist5Color("white")
+        setDist5TextColor("#ff1a44")
+      }
+    }
+
+    // !--------------------- DIETARY --------------------!
+    // Gluten Free 
+    const [diet1Color, setDiet1Color] = useState("white")
+    const [diet1TextColor, setDiet1TextColor] = useState("#ff1a44")
+    const [diet1Selected, setDiet1Selected] = useState(false)
+    const SelectDiet1 = () => {
+      setDiet1Selected(!diet1Selected)
+      
+      if(diet1Selected === false){
+        setDiet1Color("#ff1a44")
+        setDiet1TextColor("white")
+      } else {
+        setDiet1Color("white")
+        setDiet1TextColor("#ff1a44")
+      }
+    }
+    // Dairy Free
+    const [diet2Color, setDiet2Color] = useState("white")
+    const [diet2TextColor, setDiet2TextColor] = useState("#ff1a44")
+    const [diet2Selected, setDiet2Selected] = useState(false)
+    const SelectDiet2 = () => {
+      setDiet2Selected(!diet2Selected)
+      
+      if(diet2Selected === false){
+        setDiet2Color("#ff1a44")
+        setDiet2TextColor("white")
+      } else {
+        setDiet2Color("white")
+        setDiet2TextColor("#ff1a44")
+        console.log(hello)
+      }
+    }
+    // Nut Free
+    const [diet3Color, setDiet3Color] = useState("white")
+    const [diet3TextColor, setDiet3TextColor] = useState("#ff1a44")
+    const [diet3Selected, setDiet3Selected] = useState(false)
+    const SelectDiet3 = () => {
+      setDiet3Selected(!diet3Selected)
+      
+      if(diet3Selected === false){
+        setDiet3Color("#ff1a44")
+        setDiet3TextColor("white")
+      } else {
+        setDiet3Color("white")
+        setDiet3TextColor("#ff1a44")
+      }
+    }
+    // Vegetarien
+    const [diet4Color, setDiet4Color] = useState("white")
+    const [diet4TextColor, setDiet4TextColor] = useState("#ff1a44")
+    const [diet4Selected, setDiet4Selected] = useState(false)
+    const SelectDiet4 = () => {
+      setDiet4Selected(!diet4Selected)
+      
+      if(diet4Selected === false){
+        setDiet4Color("#ff1a44")
+        setDiet4TextColor("white")
+      } else {
+        setDiet4Color("white")
+        setDiet4TextColor("#ff1a44")
+      }
+    }
+
+    // !--------------------- TIME --------------------!
+    // Now
+    const [time1Color, setTime1Color] = useState("white")
+    const [time1TextColor, setTime1TextColor] = useState("#ff1a44")
+    const [time1Selected, setTime1Selected] = useState(false)
+    const SelectTime1 = () => {
+      setTime1Selected(!time1Selected)
+      
+      if(time1Selected === false){
+        setTime1Color("#ff1a44")
+        setTime1TextColor("white")
+      } else {
+        setTime1Color("white")
+        setTime1TextColor("#ff1a44")
+      }
+    }
+    // 30 min
+    const [time2Color, setTime2Color] = useState("white")
+    const [time2TextColor, setTime2TextColor] = useState("#ff1a44")
+    const [time2Selected, setTime2Selected] = useState(false)
+    const SelectTime2 = () => {
+      setTime2Selected(!time2Selected)
+      
+      if(time2Selected === false){
+        setTime2Color("#ff1a44")
+        setTime2TextColor("white")
+      } else {
+        setTime2Color("white")
+        setTime2TextColor("#ff1a44")
+      }
+    }
+    // 1 hour
+    const [time3Color, setTime3Color] = useState("white")
+    const [time3TextColor, setTime3TextColor] = useState("#ff1a44")
+    const [time3Selected, setTime3Selected] = useState(false)
+    const SelectTime3 = () => {
+      setTime3Selected(!time3Selected)
+      
+      if(time3Selected === false){
+        setTime3Color("#ff1a44")
+        setTime3TextColor("white")
+      } else {
+        setTime3Color("white")
+        setTime3TextColor("#ff1a44")
+      }
+    }
+    // 2 hours
+    const [time4Color, setTime4Color] = useState("white")
+    const [time4TextColor, setTime4TextColor] = useState("#ff1a44")
+    const [time4Selected, setTime4Selected] = useState(false)
+    const SelectTime4 = () => {
+      setTime4Selected(!time4Selected)
+      
+      if(time4Selected === false){
+        setTime4Color("#ff1a44")
+        setTime4TextColor("white")
+      } else {
+        setTime4Color("white")
+        setTime4TextColor("#ff1a44")
+      }
+    }
+    // 3 hours +
+    const [time5Color, setTime5Color] = useState("white")
+    const [time5TextColor, setTime5TextColor] = useState("#ff1a44")
+    const [time5Selected, setTime5Selected] = useState(false)
+    const SelectTime5 = async() => {
+      setTime5Selected(!time5Selected)
+      const timeresult = await axios.get('/listed.php');
+        setListedData(timeresult.data);
+      if(time5Selected === false){
+        setTime5Color("#ff1a44")
+        setTime5TextColor("white")
+      } else {
+        setTime5Color("white")
+        setTime5TextColor("#ff1a44")
+      }
+    }
+
+    // ----- FILTERS END ------
 
 
   return (
@@ -324,7 +610,62 @@ export default function Home({
           </View>
         </Modal>
         {mealtab === true && <View style={{display:'flex', justifyContent:'center', alignItems:'center', marginBottom:20}}>
-          <Filters/>
+          <Filters
+          SelectCuisine1={SelectCuisine1}
+          SelectCuisine2={SelectCuisine2}
+          SelectCuisine3={SelectCuisine3}
+          SelectCuisine4={SelectCuisine4}
+          SelectDistance1={SelectDistance1}
+          SelectDistance2={SelectDistance2}
+          SelectDistance3={SelectDistance3}
+          SelectDistance4={SelectDistance4}
+          SelectDistance5={SelectDistance5}
+          SelectDiet1={SelectDiet1}
+          SelectDiet2={SelectDiet2}
+          SelectDiet3={SelectDiet3}
+          SelectDiet4={SelectDiet4}
+          SelectTime1={SelectTime1}
+          SelectTime2={SelectTime2}
+          SelectTime3={SelectTime3}
+          SelectTime4={SelectTime4}
+          SelectTime5={SelectTime5}
+          c1TextColor={c1TextColor}
+          c2TextColor={c2TextColor}
+          c3TextColor={c3TextColor}
+          c4TextColor={c4TextColor}
+          c1Color={c1Color}
+          c2Color={c2Color}
+          c3Color={c3Color}
+          c4Color={c4Color}
+          dist1TextColor={dist1TextColor}
+          dist2TextColor={dist2TextColor}
+          dist3TextColor={dist3TextColor}
+          dist4TextColor={dist4TextColor}
+          dist5TextColor={dist5TextColor}
+          dist1Color={dist1Color}
+          dist2Color={dist2Color}
+          dist3Color={dist3Color}
+          dist4Color={dist4Color}
+          dist5Color={dist5Color}
+          diet1TextColor={diet1TextColor}
+          diet2TextColor={diet2TextColor}
+          diet3TextColor={diet3TextColor}
+          diet4TextColor={diet4TextColor}
+          diet1Color={diet1Color}
+          diet2Color={diet2Color}
+          diet3Color={diet3Color}
+          diet4Color={diet4Color}
+          time1TextColor={time1TextColor}
+          time2TextColor={time2TextColor}
+          time3TextColor={time3TextColor}
+          time4TextColor={time4TextColor}
+          time5TextColor={time5TextColor}
+          time1Color={time1Color}
+          time2Color={time2Color}
+          time3Color={time3Color}
+          time4Color={time4Color}
+          time5Color={time5Color}
+          />
 
         </View>}
         <View style={{width:'100%', alignItems:'center', paddingBottom:105}}>
@@ -334,7 +675,7 @@ export default function Home({
                key={listed.lid}
                meal={listed.m_name}
                modifications={listed.modifications}
-               restaurant={listed.restaurant}
+               restaurant={listed.full_name}
                oldprice={listed.old_price}
                newprice={listed.new_price}
                description={listed.description}
@@ -345,9 +686,10 @@ export default function Home({
                addToCart={() => {
                 setModalVisible(true)
                 setMeal(listed.m_name)
-                setRestaurant(listed.restaurant)
+                setRestaurant(listed.full_name)
                 setOldPrice(listed.old_price)
                 setNewPrice(listed.new_price)
+                setName(listed.full_name)
                 setCartItems([
                   ...cartItems, 
                   listed
